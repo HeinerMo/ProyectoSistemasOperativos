ebf32c107954357c4722f9601938627400f2c534
Heiner <heinermongelopez@gmail.com>
2021-11-18 14:23:01 -0600

    fix comments

--- initializer.c
+++ initializer.c
@@ -11,20 +11,15 @@
 #include <semaphore.h>
 #include <string.h>
 #include <stdlib.h>
-<<<<<<< HEAD
-=======
 #include <sys/mman.h>
 #include <sys/stat.h>        /* For mode constants */
 #include <fcntl.h>           /* For O_* constants */
 #include <sys/types.h>
->>>>>>> origin/toni-branch
 
 // Arguments:
 // -s [buffer size]
 // -n [buffer name]
 
-<<<<<<< HEAD
-=======
 #define SIZEOF_SMOBJ 200
 char * buffName;
 int buffSize;
@@ -67,62 +62,28 @@ void createSharedMemory () {
    	close(fd);
 
 } // Fin de createSharedMemory
->>>>>>> origin/toni-branch
 
 int main(int argc, char *argv[]) {
 
 	puts("Initializer.c running...");
 	pid_t pid = getpid();//Get process PID
-<<<<<<< HEAD
-	printf("pid: %lu\n", pid);//prints PID
-=======
 	printf("pid: %u\n", pid);//prints PID
->>>>>>> origin/toni-branch
 
 	//If the user enters -help show the argument sintax	
 	if (strcmp(argv[1], "-help") == 0) {
 		puts("Parameters: \n -n \"buffer name\" -s \"[buffer size]\"");
 		return 0;
-<<<<<<< HEAD
-	}
-=======
 	} // if
->>>>>>> origin/toni-branch
 
 	//if there are too few arguments exit
 	if (argc < 5) {
 		puts("ERROR: Too few arguments. Type -help to get the list of parameters");
 		return 1;
-<<<<<<< HEAD
-	}
-	
-	//Decode arguments
-	
-	int buffSize; //BufferSize
-	char *buffname; //bufferName
-=======
 	} // if
 	
 	//Decode arguments
 	checkParameters(argc, argv);
 	createSharedMemory();
->>>>>>> origin/toni-branch
 
-	for (int i = 1; i < argc; i++) {
-		printf("Valor %i: %s\n", i, argv[i]); 
-		if (strcmp(argv[i], "-s") == 0) {
-			if (argv[i+1] != NULL) {
-				buffSize = strtol(argv[i+1], NULL, 10);
-				printf("Buffer size: %i");
-			}
-		}
-		
-		if (strcmp(argv[i], "-n") == 0) {
-			if (argv[i+1] != NULL) {
-				buffname = argv[i+1];
-				printf("Buffer name: %s\n", buffname);
-			}
-		}
-	}
 	return 0;
 } // Fin de main
