f9d41c868d55ed35647c6ece0f314f984dba7a3a
Sojo Castro <sojos204@gmail.com>
2021-11-17 19:03:27 -0600

    Crear memoria compartida

--- initializer.c
+++ initializer.c
@@ -5,56 +5,40 @@
 //	Susan Sojo
 //Date: November 2021
 
-
 #include <stdio.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <semaphore.h>
-#include <string.h>
 #include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <unistd.h>
+#include <sys/types.h>
 
-// Arguments:
-// -s [buffer size]
-// -n [buffer name]
+#define SIZEOF_SMOBJ 200
+#define SMOBJ_NAME  "prueba"
 
-
-int main(int argc, char *argv[]) {
-	puts("Initializer.c running...");
-	pid_t pid = getpid();//Get process PID
-	printf("pid: %lu\n", pid);//prints PID
-
-	//If the user enters -help show the argument sintax	
-	if (strcmp(argv[1], "-help") == 0) {
-		puts("Parameters: \n -n \"buffer name\" -s \"[buffer size]\"");
-		return 0;
-	}
-
-	//if there are too few arguments exit
-	if (argc < 5) {
-		puts("ERROR: Too few arguments. Type -help to get the list of parameters");
-		return 1;
-	}
-	
-	//Decode arguments
-	
-	int buffSize; //BufferSize
-	char *buffname; //bufferName
-
-	for (int i = 1; i < argc; i++) {
-		printf("Valor %i: %s\n", i, argv[i]); 
-		if (strcmp(argv[i], "-s") == 0) {
-			if (argv[i+1] != NULL) {
-				buffSize = strtol(argv[i+1], NULL, 10);
-				printf("Buffer size: %i");
-			}
-		}
-		
-		if (strcmp(argv[i], "-n") == 0) {
-			if (argv[i+1] != NULL) {
-				buffname = argv[i+1];
-				printf("Buffer name: %s\n", buffname);
-			}
-		}
-	}
-	return 0;
+int main(void)
+{
+   int fd;
+   fd = shm_open (SMOBJ_NAME, O_CREAT | O_RDWR  , 00700); /* create s.m object*/
+   if(fd == -1)
+   {
+       printf("Error file descriptor \n");
+	   exit(1);
+   }
+   if(-1 == ftruncate(fd, SIZEOF_SMOBJ))
+   {
+       printf("Error shared memory cannot be resized \n");
+	   exit(1);
+   }
+   
+   close(fd);
+   
+   return 0;
 }
+
+
+
+
+
+
+
--- producer.c
+++ producer.c
@@ -5,11 +5,41 @@
 //	Susan Sojo
 //Date: November 2021
 
-
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>        /* For mode constants */
+#include <fcntl.h>           /* For O_* constants */
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
+ 
+#define SMOBJ_NAME  "prueba"
 
-
-int main(int argc, char *argv[]) {
-	puts("Producer.c running...");
-	return 0;
+int main(void)
+{
+   int fd;
+   char buf[] = "Hello,this is writting process";
+   char *ptr;
+   
+   fd = shm_open (SMOBJ_NAME,  O_RDWR  , 00200); /* open s.m object*/
+   if(fd == -1)
+   {
+       printf("Error file descriptor %s\n", strerror(errno));
+	   exit(1);
+   }
+   
+   ptr = mmap(NULL, sizeof(buf), PROT_WRITE, MAP_SHARED, fd, 0);
+   if(ptr == MAP_FAILED)
+   {
+      printf("Map failed in write process: %s\n", strerror(errno));
+      exit(1);
+   }
+   
+   memcpy(ptr,buf, sizeof(buf));
+   printf("%d \n", (int)sizeof(buf));
+   close(fd);
+   
+   return 0;
 }
